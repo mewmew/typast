f6e5c49e6926b1987ae7ef52fd95d8b9042b83b4
Robin Eklind <rnd0x00@gmail.com>
2025-08-29 23:29:39 +0200

    internal/stack: copy without modification from golang-ds/stack@5e72be38b4129c128396df023103b709fa966d3c
    BSD-3-Clause license.

---
+++ internal/stack/stack.go
@@ -0,0 +1,65 @@
+// ref: https://github.com/golang-ds/stack (license: BSD-3-Clause)
+
+// package stack implements a generic stack using a slice as the underlying data structure.
+package stack
+
+import "fmt"
+
+type SliceStack[T any] struct {
+	data []T
+}
+
+// New constructs and returns an empty slice-stack.
+// time-complexity: O(1)
+func New[T any]() SliceStack[T] {
+	return SliceStack[T]{data: []T{}}
+}
+
+// Push adds an element on top of the stack.
+// time-complexity: amortized O(1)
+func (s *SliceStack[T]) Push(data T) {
+	s.data = append(s.data, data)
+}
+
+// Pop removes and returns the top element of the stack. It returns false if the stack was empty.
+// time-complexity: O(1)
+func (s *SliceStack[T]) Pop() (val T, ok bool) {
+	if s.IsEmpty() {
+		return
+	}
+
+	val = s.data[len(s.data)-1]
+
+	s.data = s.data[:len(s.data)-1]
+
+	return val, true
+}
+
+// Top returns the top element of the stack. It returns false if the stack was empty.
+// time-complexity: O(1)
+func (s *SliceStack[T]) Top() (val T, ok bool) {
+	if s.IsEmpty() {
+		return
+	}
+
+	return s.data[len(s.data)-1], true
+}
+
+// Size returns the number of the elements in the stack.
+// time-complexity: O(1)
+func (s *SliceStack[T]) Size() int {
+	return len(s.data)
+}
+
+// IsEmpty returns true if the stack doesn't have any elements.
+// time-complexity: O(1)
+func (s *SliceStack[T]) IsEmpty() bool {
+	return s.Size() == 0
+}
+
+// String returns the string representation of the stack.
+// time-complexity: O(n)
+func (s *SliceStack[T]) String() string {
+	return fmt.Sprint(s.data)
+}
+
